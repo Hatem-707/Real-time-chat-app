692645da1bee0fe9cd7b2065563b0d6530099565
Hatem-707 <hatemfeckry347@gmail.com>
2024-12-25 11:18:41 +0200

    finalized the themes using css variables

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,10 @@ if (morning) {
   document.documentElement.style.setProperty(
     "--theme-color", morning ? "rgba(63.5,76.1,75.7,0.7)":"rgba(19.6, 20.4, 28.2, 0.7)"
   )
+  document.documentElement.style.setProperty(
+    "--field-color", morning ? "rgba(6.7, 12.2, 15.7, 0.773)" : "rgba(66, 66, 92, 0.773)"
+  )
+
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
