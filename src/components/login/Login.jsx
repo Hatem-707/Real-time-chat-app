6fbfc4bc782c49423dcec7756ceeaca7d058f3e0
Hatem-707 <hatemfeckry347@gmail.com>
2024-12-26 23:59:14 +0200

    minor adjustment to login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,40 +1,46 @@
-import { useState } from "react"
-import "./login.css"
+import { useState } from "react";
+import "./login.css";
 
 const Login = () => {
-  document.documentElement.style.setProperty(
-    "--theme-color", "transparent"
-  )
+  document.documentElement.style.setProperty("--theme-color", "transparent");
   const [avatar, setAvatar] = useState({
     file: null,
-    url: ""
-  })
+    url: "",
+  });
 
-  const handleAvatar = (e) =>{
-    if(e.target.files[0]){
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
       setAvatar({
         file: e.target.files[0],
-        url: URL.createObjectURL(e.target.files[0])
-      })
+        url: URL.createObjectURL(e.target.files[0]),
+      });
     }
-  }
+  };
 
   return (
-    <div className='main'>
-      <div className="login"> 
+    <div className="main">
+      <div className="login">
         <h1>Welcome back!</h1>
         <div className="fields">
           <span>Sign in</span>
           <div className="inner">
-          <form>
-            <input type="text" placeholder="Enter your email" name="email"></input>
-            <input type="password" placeholder="Enter your passowrd" name="password"></input>
-            <button>Sign in</button>
+            <form>
+              <input
+                type="text"
+                placeholder="Enter your email"
+                name="email"
+              ></input>
+              <input
+                type="password"
+                placeholder="Enter your passowrd"
+                name="password"
+              ></input>
+              <button>Sign in</button>
             </form>
           </div>
         </div>
       </div>
-      <div className="register"> 
+      <div className="register">
         <h1>First time?</h1>
         <h1>Don&apos;t be a stranger</h1>
         <div className="fields">
@@ -42,18 +48,36 @@ const Login = () => {
           <div className="inner">
             <form>
               <label htmlFor="image">
-                <img src={avatar.url || "/avatar.png"}/>
+                <img src={avatar.url || "/avatar.png"} />
               </label>
-              <input type="file" id = "image" style={{display:"none"}} onChange={handleAvatar}></input>
-              <input type="text" placeholder="Enter your username" name="username"></input>
-              <input type="text" placeholder="Enter your email" name="email"></input>
-              <input type="password" placeholder="Enter your passowrd" name="pasword"></input>
+              <input
+                type="file"
+                id="image"
+                style={{ display: "none" }}
+                onChange={handleAvatar}
+              ></input>
+              <input
+                type="text"
+                placeholder="Enter your username"
+                name="username"
+              ></input>
+              <input
+                type="text"
+                placeholder="Enter your email"
+                name="email"
+              ></input>
+              <input
+                type="password"
+                placeholder="Enter your passowrd"
+                name="pasword"
+              ></input>
               <button>Register</button>
             </form>
           </div>
-        </div></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
